a18781b402931dcb38a918603d1e0ce573a00fdb
Redvoid <redvoid@protonmail.com>
2018-02-17 19:49:22 +0500

    Update

--- src/commands/address.js
+++ src/commands/address.js
@@ -1,13 +1,13 @@
 const { OOPS_TEXT } = require('../messages')
 
-const ADDRESS_TEXT = 'You can send dogecoin to this address : '
+const ADDRESS_TEXT = 'You can send ich to this address : '
 
-function address (message, dogecoinNode) {
+function address (message, instacashNode) {
   var account = message.author.tag.replace('#', '')
 
   // Will create a new account if doesn't exist... ? Should we allow this ?
   // Yes
-  dogecoinNode.getAccountAddress(account, function (err, address) {
+  instacashNode.getAccountAddress(account, function (err, address) {
     if (err) {
       console.log(err)
       message.channel.send(OOPS_TEXT)
