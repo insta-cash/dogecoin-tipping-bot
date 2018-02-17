1a5233928dbecdaba21b850d3ca5ada0361a2ddb
Redvoid <redvoid@protonmail.com>
2018-02-17 19:52:19 +0500

    Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,16 +1,12 @@
-const { GIPHY_KEY } = require('../settings')
-
-const HELP_TEXT = '```Wow. DogeCoin to moon. \n \
+const HELP_TEXT = '```Below are the commands available \n \
 Command list : \n \
   - **help** : Get help ! \n \
-  - **tip** : Tip someone some dogecoin \n \
+  - **tip** : Tip someone ICH \n \
   - **balance** : Get your balance \n \
-  - **rate** : Get current value \n \
   - **address** : Show an used wallet so you can refill your wallet \n \
   - **withdraw** : Move coins to an external address \n \
-  - **qrcode** : Show your qrcode to receive much money \n \
-' + ((GIPHY_KEY !== null && GIPHY_KEY !== '') ? '  - **goodboy** : Wow, much doggo, such reward, wow \n' : '') + ' \
-```'
+  - **qrcode** : Show your qrcode to receive ICH \n \
+
 
 function help(message) {
   message.channel.send(HELP_TEXT)
