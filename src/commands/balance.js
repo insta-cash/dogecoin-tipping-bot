0e3b8be72f98ee16ccb189f7319299346bc1d8a4
Redvoid <redvoid@protonmail.com>
2018-02-17 19:50:20 +0500

    Update balance.js

--- src/commands/balance.js
+++ src/commands/balance.js
@@ -1,19 +1,19 @@
 const { OOPS_TEXT } = require('../messages')
-const { rateDogeEur } = require('../requests')
+const { rateICHEur } = require('../requests')
 
-const BALANCE_TEXT = 'Wow. Balance : '
+const BALANCE_TEXT = 'Balance : '
 
-function balance (message, dogecoinNode) {
+function balance (message, instacashNode) {
   var account = message.author.tag.replace('#', '')
 
-  dogecoinNode.getBalance(account, function (err, balance) {
+  instacashNode.getBalance(account, function (err, balance) {
     if (err) {
       console.log(err)
       message.channel.send(OOPS_TEXT)
       return
     }
 
-    rateDogeEur().then((rate) => {
+    rateICHEur().then((rate) => {
       message.channel.send(BALANCE_TEXT + (balance).toFixed(2) + ' DOGE ( ' + (balance * rate).toFixed(2) + ' EUR )')
     }).catch((error) => {
       message.channel.send(error)
