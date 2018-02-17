4b77341b0fbefc20deacbb50d104ef68719a8c3d
Redvoid <redvoid@protonmail.com>
2018-02-17 19:52:42 +0500

    Update index.js

--- src/commands/index.js
+++ src/commands/index.js
@@ -4,7 +4,6 @@ const balance = require('./balance')
 const rate = require('./rate')
 const address = require('./address')
 const withdraw = require('./withdraw')
-const goodboy = require('./goodboy')
 const qrcode = require('./qrcode')
 
-module.exports = { help, tip, balance, rate, address, withdraw, goodboy, qrcode }
+module.exports = { help, tip, balance, rate, address, withdraw, qrcode }
